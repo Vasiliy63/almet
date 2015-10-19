0d1f671e134d6726914c5acbd51c70b6a1ac62e2
Vasiliy <vasiliysidorenko63@gmail.com>
2015-10-19 23:39:02 +0400

    add outdatebower

---
+++ javascript/outdatedbrowser.js
@@ -0,0 +1,9 @@
+
+
+//DOM ready or jQuery
+    outdatedBrowser({
+        bgColor: 'rgba(0, 0, 0, 0.9)',
+        color: '#ffffff',
+        lowerThan: 'transform',
+        languagePath: 'bower_components/outdated-browser/outdatedbrowser/lang/ru.html'
+    })
